9b68228dd5c0585cb09a89eeb619d57b14a2ce9d
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-04-01 12:56:57 -0500

    last ver

--- p2.cpp
+++ p2.cpp
@@ -4,11 +4,15 @@
 
 using namespace std;
 
+
 struct Alumno {
     char codigo[5];
     char nombre[11];
     char apellidos[20];
     char carrera[15];
+
+    int ciclo;
+    float mensualidad;
 };
 
 void readFromConsole(char buffer[], int size) {
@@ -21,24 +25,14 @@ void readFromConsole(char buffer[], int size) {
 }
 
 ostream &operator<<(ostream &stream, Alumno &p) {
-    stream << p.codigo << " ";
-    stream << p.nombre << " ";
-    stream << p.apellidos << " ";
-    stream << p.carrera << " ";
+    stream.write((char *) &p, sizeof(p));
     stream << "\n";
     stream << flush;
     return stream;
 }
 
 istream &operator>>(istream &stream, Alumno &p) {
-    stream.read(p.codigo, 5);
-    p.codigo[4] = '\0';
-    stream.read(p.nombre, 11);
-    p.nombre[10] = '\0';
-    stream.read(p.apellidos, 20);
-    p.apellidos[19] = '\0';
-    stream.read(p.carrera, 15);
-    p.carrera[14] = '\0';
+    stream.read((char *) &p, sizeof(p));
     stream.get();
     return stream;
 }
@@ -52,7 +46,7 @@ public:
 
     vector<Alumno> load() {
         ifstream infile;
-        infile.open(filename);
+        infile.open(filename, ios::binary);
         vector<Alumno> alumnos;
 
         if (infile.is_open()) {
@@ -70,7 +64,7 @@ public:
 
     void add(Alumno record) {
         ofstream outFile;
-        outFile.open(filename, ios::app);
+        outFile.open(filename, ios::app | ios::binary);
         if (outFile.is_open()) {
             outFile << record;
             outFile.close();
@@ -93,30 +87,5 @@ public:
     }
 };
 
-
 int main() {
-    FixedRecord fixedRecord("../datos1.txt");
-    vector<Alumno> alumnos = fixedRecord.load();
-    for (auto &tmp: alumnos) {
-        cout << "Codigo:" << tmp.codigo << endl;
-        cout << "Nombre:" << tmp.nombre << endl;
-        cout << "Apellidos:" << tmp.apellidos << endl;
-        cout << "Carrera:" << tmp.carrera << endl;
-    }
-        Alumno alumno;
-        cout << "Codigo: ";
-        readFromConsole(alumno.codigo, 5);
-        cout << "Nombre: ";
-        readFromConsole(alumno.nombre, 11);
-        cout << "Apellidos: ";
-        readFromConsole(alumno.apellidos, 20);
-        cout << "Carrera: ";
-        readFromConsole(alumno.carrera, 15);
-        fixedRecord.add(alumno);
-        Alumno segundo = fixedRecord.readRecord(1);
-        cout << "Codigo:" << segundo.codigo << endl;
-        cout << "Nombre:" << segundo.nombre << endl;
-        cout << "Apellidos:" << segundo.apellidos << endl;
-        cout << "Carrera:" << segundo.carrera << endl;
-    return 0;
 }
