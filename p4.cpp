cb4cbef7c8cc95f078e85533f04de3fdc8f1987c
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-04-04 21:58:00 -0500

    added implementation using stringstreams, found bug, istringstream only considers char * until the first \0 (the one in codigo)

--- p4.cpp
+++ p4.cpp
@@ -20,47 +20,68 @@ struct Matricula {
         return sizeof(codigo.length() + 1) + sizeof(string) + sizeof(int) + sizeof(float) + sizeof(observaciones.length() + 1) + sizeof(string);
     }
 
-    char *zip() {
-        char *buffer = new char[recordSize()];
-//        ostringstream outStringStream(buffer, ios::binary);
-//        cout << "pos:" << outStringStream.tellp() << endl;
-//        auto codigoLength = codigo.length();
-//        outStringStream.write((char *) &codigoLength, sizeof(codigoLength));
-//        outStringStream.write(codigo.c_str(), codigoLength + 1);
-//        outStringStream.write((char *) &ciclo, sizeof(int));
-//        outStringStream.write((char *) &mensualidad, sizeof(float));
-//        auto observacionesLength = observaciones.length();
-//        outStringStream.write((char *) &observacionesLength, sizeof(observacionesLength));
-//        outStringStream.write(observaciones.c_str(), observacionesLength + 1);
-//        cout << "pos:" << outStringStream.tellp() << endl;
-//        int i;
-        char *tmpPtr = buffer;
+    ostringstream zip() {
+        char *charBuf = new char[recordSize()];
+        ostringstream buffer(charBuf, ios::binary);
+        cout << "pos:" << buffer.tellp() << endl;
         string::size_type codigoLength = codigo.length();
-        sprintf_s(tmpPtr, sizeof(codigoLength), "%s", (char *) &codigoLength);
-        tmpPtr += sizeof(codigoLength);
-        sprintf_s(tmpPtr, codigoLength + 1, "%s", (char *) codigo.c_str());
-        tmpPtr += codigoLength + 1;
-        sprintf_s(tmpPtr, sizeof(int), "%s", (char *) &ciclo);
-        tmpPtr += sizeof(int);
-        sprintf_s(tmpPtr, sizeof(float) , "%s", (char *) &mensualidad);
-        tmpPtr += sizeof(float);
-        auto observacionesLength = observaciones.length();
-        sprintf_s(tmpPtr, sizeof(observacionesLength), "%s", (char *) &observacionesLength);
-        tmpPtr += sizeof(observacionesLength);
-        sprintf_s(tmpPtr, observacionesLength + 1, "%s", (char *) observaciones.c_str());
+        buffer.write((char *) &codigoLength, sizeof(codigoLength));
+        cout << "content: " << buffer.str() << endl;
+        buffer.write(codigo.c_str(), codigoLength + 1);
+        cout << "content: " << buffer.str() << endl;
+        buffer.write((char *) &ciclo, sizeof(int));
+        cout << "content: " << buffer.str() << endl;
+        buffer.write((char *) &mensualidad, sizeof(float));
+        cout << "content: " << buffer.str() << endl;
+        string::size_type observacionesLength = observaciones.length();
+        buffer.write((char *) &observacionesLength, sizeof(observacionesLength));
+        cout << "content: " << buffer.str() << endl;
+        buffer.write(observaciones.c_str(), observacionesLength + 1);
+        cout << "content: " << buffer.str() << endl;
+        cout << "pos:" << buffer.tellp() << endl;
+        cout << "real size:" << recordSize() << endl;
+//        int i;
+//        char *tmpPtr = buffer;
+//        string::size_type codigoLength = codigo.length();
+//        cout << "wrote: " << sprintf_s(tmpPtr, sizeof(codigoLength), "%s", (char *) &codigoLength) << endl;
+//        cout << "content: "<< tmpPtr << endl;
+//        tmpPtr += sizeof(codigoLength);
+//        cout << "wrote: " << sprintf_s(tmpPtr, codigoLength + 1, "%s", (char *) codigo.c_str())<< endl;
+//        cout << "content: "<< tmpPtr << endl;
+//        tmpPtr += codigoLength + 1;
+//        cout << "wrote: " << sprintf_s(tmpPtr, sizeof(int), "%s", (char *) &ciclo)<< endl;
+//        cout << "content: "<< tmpPtr << endl;
+//        tmpPtr += sizeof(int);
+//        cout << "wrote: " << sprintf_s(tmpPtr, sizeof(float) , "%s", (char *) &mensualidad)<< endl;
+//        cout << "content: "<< tmpPtr << endl;
+//        tmpPtr += sizeof(float);
+//        auto observacionesLength = observaciones.length();
+//        cout << "wrote: " << sprintf_s(tmpPtr, sizeof(observacionesLength), "%s", (char *) &observacionesLength)<< endl;
+//        cout << "content: "<< tmpPtr << endl;
+//        tmpPtr += sizeof(observacionesLength);
+//        cout << "wrote: " << sprintf_s(tmpPtr, observacionesLength + 1, "%s", (char *) observaciones.c_str())<< endl;
+//        cout << "content: "<< tmpPtr << endl;
 //        cout << "i:" << i << endl;
 //        printf_s( "Output:\n%s\ncharacter count = %d\n", buffer, i);
         return buffer;
     }
 
-    void unzip(char *zippedRecord, size_t zippedRecordSize) {
-        istringstream strbuff(zippedRecord);
+    void unzip(char *packedRegister) {
+//        char *charBuf = new char[recordSize()];
+        istringstream buffer(packedRegister, ios::binary);
+        cout << packedRegister << endl;
+        buffer.seekg(0);
         string::size_type codigoLength;
-        strbuff.read((char *) &codigoLength, sizeof(codigoLength));
-        cout << codigoLength << endl;
+        cout << buffer.tellg() << endl;
+        buffer.read((char *) &codigoLength, sizeof(codigoLength));
+//        buffer.seekg(sizeof(codigoLength) + 1);
+        cout << "codigo length: "<< codigoLength << endl;
+        cout << buffer.tellg() << endl;
         char *codigoBuffer = new char[codigoLength+1];
-        strbuff.read(codigoBuffer, codigoLength + 1);
+        buffer.read(codigoBuffer, codigoLength);
+        cout << buffer.tellg() << endl;
         codigoBuffer[codigoLength] = 0;
+        cout <<"codigo buffer: "<< codigoBuffer << endl;
         codigo = codigoBuffer;
 
     }
@@ -79,12 +100,12 @@ public:
 
 int main() {
     Matricula matricula("0001", 1, 5500.2, "Es alto");
-    char *zip = matricula.zip();
-    for (int i = 0; i < matricula.recordSize(); ++i) {
-        cout << zip[i];
-    }
-    cout << endl << matricula.recordSize();
+    auto zip = matricula.zip();
+    cout << zip.str() << endl;
+    cout << endl << matricula.recordSize() << endl;
     Matricula matricula2;
-    matricula2.unzip(zip, matricula.recordSize());
+    auto tmp = zip.str();
+    char *c = (char *) tmp.c_str();
+    matricula2.unzip(c);
     cout << "codigo:" << matricula2.codigo << endl;
 }
