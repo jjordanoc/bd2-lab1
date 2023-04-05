303acc531d4fac90d7fc27d7fa376322e91ccd96
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-04-04 19:44:50 -0500

    p4 fix

--- p4.cpp
+++ p4.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include  <sstream>
 #include <vector>
 using namespace std;
 
@@ -16,37 +17,52 @@ struct Matricula {
     Matricula(const string &codigo, int ciclo, float mensualidad, const string &observaciones) : codigo(codigo), ciclo(ciclo), mensualidad(mensualidad), observaciones(observaciones) {}
 
     size_t recordSize() {
-        return sizeof(codigo.length()) + sizeof(string) + sizeof(int) + sizeof(float) + sizeof(observaciones.length()) + sizeof(string);
+        return sizeof(codigo.length() + 1) + sizeof(string) + sizeof(int) + sizeof(float) + sizeof(observaciones.length() + 1) + sizeof(string);
     }
 
     char *zip() {
         char *buffer = new char[recordSize()];
+//        ostringstream outStringStream(buffer, ios::binary);
+//        cout << "pos:" << outStringStream.tellp() << endl;
+//        auto codigoLength = codigo.length();
+//        outStringStream.write((char *) &codigoLength, sizeof(codigoLength));
+//        outStringStream.write(codigo.c_str(), codigoLength + 1);
+//        outStringStream.write((char *) &ciclo, sizeof(int));
+//        outStringStream.write((char *) &mensualidad, sizeof(float));
+//        auto observacionesLength = observaciones.length();
+//        outStringStream.write((char *) &observacionesLength, sizeof(observacionesLength));
+//        outStringStream.write(observaciones.c_str(), observacionesLength + 1);
+//        cout << "pos:" << outStringStream.tellp() << endl;
 //        int i;
         char *tmpPtr = buffer;
-        auto codigoLength = codigo.length();
-//        i = sprintf_s(buffer, sizeof(codigoLength), "%s", (char *) &codigoLength);
-        sprintf_s(tmpPtr, codigoLength, "%s", (char *) codigo.c_str());
-        tmpPtr += codigoLength;
+        string::size_type codigoLength = codigo.length();
+        sprintf_s(tmpPtr, sizeof(codigoLength), "%s", (char *) &codigoLength);
+        tmpPtr += sizeof(codigoLength);
+        sprintf_s(tmpPtr, codigoLength + 1, "%s", (char *) codigo.c_str());
+        tmpPtr += codigoLength + 1;
         sprintf_s(tmpPtr, sizeof(int), "%s", (char *) &ciclo);
         tmpPtr += sizeof(int);
-        sprintf_s(tmpPtr, sizeof(float), "%s", (char *) &mensualidad);
+        sprintf_s(tmpPtr, sizeof(float) , "%s", (char *) &mensualidad);
         tmpPtr += sizeof(float);
         auto observacionesLength = observaciones.length();
         sprintf_s(tmpPtr, sizeof(observacionesLength), "%s", (char *) &observacionesLength);
         tmpPtr += sizeof(observacionesLength);
-        sprintf_s(tmpPtr, observacionesLength, "%s", (char *) observaciones.c_str());
+        sprintf_s(tmpPtr, observacionesLength + 1, "%s", (char *) observaciones.c_str());
 //        cout << "i:" << i << endl;
 //        printf_s( "Output:\n%s\ncharacter count = %d\n", buffer, i);
         return buffer;
     }
 
     void unzip(char *zippedRecord, size_t zippedRecordSize) {
-        int i;
-        size_t codigoLength;
-        i = sprintf_s((char *) &codigoLength, sizeof(codigoLength), "%s", zippedRecord);
-        char *codigoBuffer = new char[codigoLength];
-        i += sprintf_s(codigoBuffer, codigoLength, "%s", zippedRecord + i);
+        istringstream strbuff(zippedRecord);
+        string::size_type codigoLength;
+        strbuff.read((char *) &codigoLength, sizeof(codigoLength));
+        cout << codigoLength << endl;
+        char *codigoBuffer = new char[codigoLength+1];
+        strbuff.read(codigoBuffer, codigoLength + 1);
+        codigoBuffer[codigoLength] = 0;
         codigo = codigoBuffer;
+
     }
 };
 
